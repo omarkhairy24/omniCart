1f867b4c81588b44cf34404de7c3d0bf3e1f0b8e
omar khairy <omarkairyno7@gmail.com>
2023-11-20 07:37:17 +0200

    email.js editing

--- email.js
+++ email.js
@@ -2,8 +2,10 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async options =>{
     const transporter = nodemailer.createTransport({
+        service:process.env.SERVICE,
         host:process.env.EMAIL_HOST,
         port:process.env.EMAIL_PORT,
+        secure:false,
         auth:{
             user:process.env.EMAIL_USERNAME,
             pass:process.env.EMAIL_PASSWORD
